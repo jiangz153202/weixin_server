e0ae19c7155a3685fb2a6d17f51bc303840bc816
jiangz153202 <709732919@qq.com>
2017-12-19 16:47:52 +0800

    update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var users = require('./routes/wechat');
+var wechat = require('./routes/wechat');
 var app = express();
 
 // view engine setup
