931d614dc5faf64abd044a8b512d0b272df3f005
jiangz153202 <709732919@qq.com>
2017-12-20 11:16:41 +0800

    logger update to console

--- routes/wechat.js
+++ routes/wechat.js
@@ -39,79 +39,79 @@ var menu = {
 //删除菜单
 api.removeMenu(function (err, result) {
   if (err) {
-    logger.log('error', err);
+    console.log('error', err);
   }
-  logger.log('info', 'remove menu success');
+  console.log('info', 'remove menu success');
 });
 
 //创建菜单
 api.createMenu(menu, function (err, result) {
   if (err) {
-    logger.log('error', err);
+    console.log('error', err);
   }
-  logger.log('info', 'create menu success');
+  console.log('info', 'create menu success');
 });
 
 
 router.use('/wechat', wechat(config, wechat.text(function (message, req, res, next) {
     //------------------------------------------------------------------------
     var message = req.weixin;
-    logger.log("info", message);
+    console.log("info", message);
   
     res.reply('Message Send To Bot Completed , Wait Response.');
     
     api.sendText(message.FromUserName, 'this message from wechat-api', function (err, result) {
         if (err) {
-          logger.log('error', err);
+          console.log('error', err);
         }
-        logger.log('info', 'reply message success');
+        console.log('info', 'reply message success');
       });
     
   }).image(function (message, req, res, next) {
     var message = req.weixin;
-    logger.log("info", message);
+    console.log("info", message);
   
     res.reply('功能开发中');
   }).voice(function (message, req, res, next) {
     var message = req.weixin;
-    logger.log("info", message);
+    console.log("info", message);
   
     res.reply('功能开发中');
   }).video(function (message, req, res, next) {
     var message = req.weixin;
-    logger.log("info", message);
+    console.log("info", message);
   
     res.reply('功能开发中');
   }).location(function (message, req, res, next) {
     var message = req.weixin;
-    logger.log("info", message);
+    console.log("info", message);
   
     res.reply('功能开发中');
   }).link(function (message, req, res, next) {
     var message = req.weixin;
-    logger.log("info", message);
+    console.log("info", message);
   
     res.reply('功能开发中');
   }).event(function (message, req, res, next) {
     var message = req.weixin;
-    logger.log("info", message);
+    console.log("info", message);
   
     res.reply('感谢你的关注，你也可以在nodejs npm中查看wechat和wechat-api');
   
   }).device_text(function (message, req, res, next) {
     var message = req.weixin;
-    logger.log("info", message);
+    console.log("info", message);
   
     res.reply('功能开发中');
   }).device_event(function (message, req, res, next) {
     if (message.Event === 'subscribe' || message.Event === 'unsubscribe') {
       var message = req.weixin;
-      logger.log("info", message);
+      console.log("info", message);
   
       res.reply("功能开发中");
     } else {
       var message = req.weixin;
-      logger.log("info", message);
+      console.log("info", message);
   
       res.reply('功能开发中');
     }
