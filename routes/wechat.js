ecb7c1f0d5d8b9d6c8dae00b28583cfb1a382dd3
jiangz153202 <709732919@qq.com>
2017-12-20 11:12:43 +0800

    wechat-api demo test

--- routes/wechat.js
+++ routes/wechat.js
@@ -4,43 +4,118 @@ var router = express.Router();
 var wechat = require('wechat');
 var config = require('../config.js');
 
-router.use('/', wechat(config, function (req, res, next) {
-    // 微信输入信息都在req.weixin上
+var wechatAPI = require('wechat-api'); 
+
+
+//创建wechat-api
+var api = new wechatAPI(config.appid, 'b09a91f59291b95323c59cc93800e9da');
+
+var menu = {
+  "button": [
+    {
+      "type": "click",
+      "name": "WeChat Bot",
+      "key": "V1001_TODAY_MUSIC"
+    },
+    {
+      "name": "BotFramework",
+      "sub_button": [
+        {
+          "type": "view",
+          "name": "botframework",
+          "url": "https://dev.botframework.com/"
+        },
+        {
+          "type": "click",
+          "name": "赞一下我们",
+          "key": "V1001_GOOD"
+        }, {
+          "name": "发送位置",
+          "type": "location_select",
+          "key": "rselfmenu_2_0"
+        },]
+    }]
+};
+//删除菜单
+api.removeMenu(function (err, result) {
+  if (err) {
+    logger.log('error', err);
+  }
+  logger.log('info', 'remove menu success');
+});
+
+//创建菜单
+api.createMenu(menu, function (err, result) {
+  if (err) {
+    logger.log('error', err);
+  }
+  logger.log('info', 'create menu success');
+});
+
+
+router.use('/wechat', wechat(config, wechat.text(function (message, req, res, next) {
+    //------------------------------------------------------------------------
     var message = req.weixin;
-    console.log('在微信上输出',JSON.stringify(req.weixin));
-    if (message.Content === 'diaosi') {
-        // 回复屌丝(普通回复)
-        res.reply('hehe');
-    } else if (message.Content === 'text') {
-        //你也可以这样回复text类型的信息
-        res.reply({
-            content: 'text object',
-            type: 'text'
-        });
-    } else if (message.Content === 'hehe') {
-        // 回复一段音乐
-        res.reply({
-            type: "music",
-            content: {
-                title: "来段音乐吧",
-                description: "一无所有",
-                musicUrl: "http://mp3.com/xx.mp3",
-                hqMusicUrl: "http://mp3.com/xx.mp3",
-                thumbMediaId: "thisThumbMediaId"
-            }
-        });
+    logger.log("info", message);
+  
+    res.reply('Message Send To Bot Completed , Wait Response.');
+    
+    api.sendText(message.FromUserName, 'this message from wechat-api', function (err, result) {
+        if (err) {
+          logger.log('error', err);
+        }
+        logger.log('info', 'reply message success');
+      });
+    
+  }).image(function (message, req, res, next) {
+    var message = req.weixin;
+    logger.log("info", message);
+  
+    res.reply('功能开发中');
+  }).voice(function (message, req, res, next) {
+    var message = req.weixin;
+    logger.log("info", message);
+  
+    res.reply('功能开发中');
+  }).video(function (message, req, res, next) {
+    var message = req.weixin;
+    logger.log("info", message);
+  
+    res.reply('功能开发中');
+  }).location(function (message, req, res, next) {
+    var message = req.weixin;
+    logger.log("info", message);
+  
+    res.reply('功能开发中');
+  }).link(function (message, req, res, next) {
+    var message = req.weixin;
+    logger.log("info", message);
+  
+    res.reply('功能开发中');
+  }).event(function (message, req, res, next) {
+    var message = req.weixin;
+    logger.log("info", message);
+  
+    res.reply('感谢你的关注，你也可以在nodejs npm中查看wechat和wechat-api');
+  
+  }).device_text(function (message, req, res, next) {
+    var message = req.weixin;
+    logger.log("info", message);
+  
+    res.reply('功能开发中');
+  }).device_event(function (message, req, res, next) {
+    if (message.Event === 'subscribe' || message.Event === 'unsubscribe') {
+      var message = req.weixin;
+      logger.log("info", message);
+  
+      res.reply("功能开发中");
     } else {
-        // 回复高富帅(图文回复)
-        res.reply([
-            {
-                title: '你来我家接我吧',
-                description: '这是女神与高富帅之间的对话',
-                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-                url: 'http://nodeapi.cloudfoundry.com/'
-            }
-        ]);
+      var message = req.weixin;
+      logger.log("info", message);
+  
+      res.reply('功能开发中');
     }
-}));
+  })));
 
 module.exports = router;
 
